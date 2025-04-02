43df99858d20c282c946185f924e61bc9e4c98b3
Henry <henrychau123@gmail.com>
2025-04-02 14:26:24 -0500

    Adds and implements a new Attacker vs Defender section.

--- PowerCompare/PowerCompare/MainWindow.xaml.cs
+++ PowerCompare/PowerCompare/MainWindow.xaml.cs
@@ -32,6 +32,8 @@ public partial class MainWindow : Window
         LabelHigherPowerR.Text = _themHigherThenText;
         
         SliderPower.ValueChanged += SliderPower_ValueChanged;
+        SliderAttackerPower.ValueChanged += SliderAttackerPower_ValueChanged;
+        SliderDefenderPower.ValueChanged += SliderDefenderPower_ValueChanged;
     }
 
     private void SliderPower_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -39,6 +41,36 @@ public partial class MainWindow : Window
         TextBoxSelectedPower.Text = e.NewValue.ToString("n0");
         TextBoxLowerPower.Text = GetLowerPowerValue(e.NewValue).ToString("n0");
         TextBoxHigherPower.Text = GetHigherPowerValue(e.NewValue).ToString("n0");
+    }
+    
+    private void SliderAttackerPower_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        LabelAttackerPower.Text = "Attacker: " + e.NewValue.ToString("n0");
+        UpdateAttackerVsDefender();
+    }
+    
+    private void SliderDefenderPower_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        LabelDefenderPower.Text = "Defender: " + e.NewValue.ToString("n0");
+        UpdateAttackerVsDefender();
+    }
+
+    private void UpdateAttackerVsDefender()
+    {
+        var attackerPower = SliderAttackerPower.Value;
+        var defenderPower = SliderDefenderPower.Value;
+        var penaltyPercentage = GetPowerPenalty(attackerPower, defenderPower);
+        
+        if (penaltyPercentage == 0)
+        {
+            LabelAttackerVsDefender.Foreground = Brushes.Green;
+            LabelAttackerVsDefender.Text = "No stat penalty.";
+        }
+        else
+        {
+            LabelAttackerVsDefender.Foreground = Brushes.Red;
+            LabelAttackerVsDefender.Text = $"Attacker has {penaltyPercentage}% stat penalty.";
+        }
     }
 
     /// <summary>
@@ -57,9 +89,6 @@ public partial class MainWindow : Window
         return Math.Round((selectedValue / 0.846) / 1000)  * 1000;
     }
     
-    /// <summary>
-    /// Rounded to nearest thousand.
-    /// </summary>
     private double GetPowerPenalty(double attackerPower, double defenderPower)
     {
         if (attackerPower > defenderPower) { return 0; }
