d643c27ca264038286ac77be1be32b0acebe5b33
Henry <henrychau123@gmail.com>
2025-04-02 00:30:40 -0500

    Adds initial UI.

--- PowerCompare/PowerCompare/MainWindow.xaml.cs
+++ PowerCompare/PowerCompare/MainWindow.xaml.cs
@@ -11,13 +11,22 @@ using System.Windows.Shapes;
 
 namespace PowerCompare;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+        SliderPower.ValueChanged += SliderValue_ValueChanged;
+    }
+
+    private void SliderValue_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        // TODO: actually implement logic here rather than the sample given below
+        
+        
+        LabelLowerPower.Text = $"Test 1: { (int)SliderPower.Value }";
+
+        int test2Value = (int)(SliderPower.Value * 0.5);
+        LabelHigherPower.Text = $"Test 2: { test2Value }";
     }
 }
